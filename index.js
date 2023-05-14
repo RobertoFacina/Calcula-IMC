943448c1f0eafcb6c75eb5574c3faafc64050dae
RobertoFacina <89604174+RobertoFacina@users.noreply.github.com>
2023-05-14 10:14:16 -0300

    Update index.js
    Adicionada interação com usuário

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
+// Calculando IMC
+
 function mostra(mensagem){
+    document.write(mensagem);
+}
+
+let nome = prompt("Informe seu nome: ");
+let altura = prompt(nome + ", informe sua altura: ");
+let peso = prompt(nome + ", informe seu peso: ");
+
+function calculaImc(){
+    let imc = peso / (altura**2);
+    mostra("O cálculo do seu IMC é: " + imc);
+    mostra("<br>Arredondado fica em: " + Math.round(imc) + "<br>");
+
+    if(imc < 18.5){
+        mostra("Você está com o peso abaixo do normal <br>");
+    }else if(imc <= 24.9){
+        mostra("Você está com o peso normal <br>");
+    }else if(imc <= 29.9){
+        mostra("Você está com excesso de peso, tome cuidado <br>");
+    }else if(imc >= 30) {
+        mostra("Você está obeso!! <br>");
+    }
+}
+
+calculaImc();
+mostra("Indicador do IMC respondido com base no número não arredondado.");
+
+/*function mostra(mensagem){
     document.write(mensagem);
 }
 function calculaImc(peso , altura){
@@ -17,34 +46,4 @@ function calculaImc(peso , altura){
     }
 }
 
-calculaImc(80,1.80);
-
-
-
-// alert("Você está muito longe de casa!")
-
-// let ano = 2000;
-
-// document.write("<h1> AQUI É O BRASIL!!!!</h1>");
-// document.write("<br>");
-// document.write("Acabei de pular uma linha :D!!");
-// document.write("<br>");
-// document.write("Agora vou concatenar as paradinhas, eis aqui um número aleatório: " + 10);
-// document.write("<br>");
-// document.write("Agora vai uma sominha: 20 + 25 = " + 45);
-// document.write("<br>");
-// document.write("Ou apenas a soma no código: " + (20 + 25));
-// document.write("<br>");
-// document.write("O ano do meu nascimento é: " + ano + "<br>");
-
-// var media = (11 + 22 + 33 + 41 + 59)/5;
-
-// document.write("Alunos entraram na turma com as idades: " + 11 + "," + 22 + "," + 33 + "," + 41 + "," + 59 + "anos. <br>");
-// document.write("A media das idades arredondada é: " + Math.round(media));
-
-// //função Math.round() serve para arredondar um número.
-
-// Calculando IMC
-
-
-
+calculaImc(80,1.80);*/
